4de0090897b2da98cfe146ac454adfebb9825eb5
Maikl-ge <503414@gmail.com>
2025-09-23 10:22:43 +0400

    arm

--- include/pinout.h
+++ include/pinout.h
@@ -28,20 +28,20 @@
 
 // Выходы для управления фермой ----------------------------
 // Пины для управления устройствами PWM
-#define LIGHT_PIN  2  // Свет (PWM) (GPIO02, нога 24)
+#define LIGHT_PIN  13  // Свет (PWM) (GPIO02, нога 24)
 #define FAN_VENT_PIN 26    // Циркуляция внутри 3 и 4 полки (PWM) (GPIO26, нога 10)
 #define FAN_CIRC_PIN 15     // Циркуляция внутри камеры (PWM) (GPIO15, нога 23)
 #define FAN_INLET_PIN 12        // Подача воздуха из вне (PWM) (GPIO12, нога 13)
 #define HITER_AIR_PIN 14    // Обогрев камеры (PWM) (GPIO13, нога 15)
-
 #define HITER_WATER_PIN 27  // Нагрев воды (PWM) (GPIO14, нога 12)
 
 // Пины для управления нагрузками ON/OFF
 #define PUMP_WATERING_PIN 32    // Полив (ON/OFF) (GPIO32, нога 8)
-#define WATER_OUT_PIN 33 // Слив (ON/OFF) (GPIO27, нога 11)
-#define STEAM_IN_PIN 17  // Парогенератор (ON/OFF) (GPI033, нога 7)
+#define WATER_OUT_PIN 17 // Слив (ON/OFF) (GPIO27, нога 11)
+#define STEAM_IN_PIN 33  // Парогенератор (ON/OFF) (GPI033, нога 7)
 
-#define PUMP_TRANSFER_PIN 13   // Подача в бак полива osmo воды (ON/OFF) (GPIO17, нога )
+
+#define PUMP_TRANSFER_PIN 2   // Подача в бак полива osmo воды (ON/OFF) (GPIO17, нога )
 #define OSMOS_ON_PIN 3 // Подача очищенной воды (ON/OFF) (GPIO03, нога 34)
 
 // Шаговый двигатель (Step, Dir, Enable)
--- src/watering.cpp
+++ src/watering.cpp
@@ -19,7 +19,7 @@ bool pumpIsOffLevel = false;
 bool drainPending = false;
 
 const unsigned long PUMP_RUN_TIME_SECONDS = 2;    //* 60;  // Время работы насоса - 1 минута
-const unsigned long DRAIN_OPEN_TIME = 4.5 * 60;      // Время открытия клапана слива - 2 минуты
+const unsigned long DRAIN_OPEN_TIME = 2 * 60;      // Время открытия клапана слива - 2 минуты
 
 bool max_watering_level = 1; // Уровень воды в баке убрать после отладки
 
